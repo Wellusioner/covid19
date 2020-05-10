012011d9e64eaaa54d6db1d381ee98fa3a5586ba
Hamdamov Doston <hamdamovdostonbek2020@gmail.com>
2020-05-10 11:31:45 +0500

    Updates

---
+++ precache-manifest.f39df0f216fde0ee8c358082aa05895d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18661dc9c9f0ad7c8c14c2df5e51fc7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3be253adff2b6ea1399b",
+    "url": "/static/css/main.44071d29.chunk.css"
+  },
+  {
+    "revision": "c24266eacf88a4af2b61",
+    "url": "/static/js/2.bbc8cc64.chunk.js"
+  },
+  {
+    "revision": "c7a7e79b10ab645cab64be742e1dd9e5",
+    "url": "/static/js/2.bbc8cc64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3be253adff2b6ea1399b",
+    "url": "/static/js/main.04efdf12.chunk.js"
+  },
+  {
+    "revision": "2873b697e63ca0e07f7d",
+    "url": "/static/js/runtime-main.8220091d.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/covid.d7265326.png"
+  },
+  {
+    "revision": "89d6413d382aee909989d73925b846fa",
+    "url": "/static/media/doctor2.89d6413d.jpg"
+  }
+]);
